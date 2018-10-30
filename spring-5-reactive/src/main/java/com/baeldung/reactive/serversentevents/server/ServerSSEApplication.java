2957c2ccc5510758dc9727ff05b3b18f023ce459
Norman Jarvis <norman.jarvis@evernym.com>
2018-10-30 16:12:46 -0600

    Disabling the default security configuration

--- spring-5-reactive/src/main/java/com/baeldung/reactive/serversentevents/server/ServerSSEApplication.java
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/serversentevents/server/ServerSSEApplication.java
@@ -4,8 +4,9 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ServerSSEApplication {
     
     public static void main(String[] args) {
